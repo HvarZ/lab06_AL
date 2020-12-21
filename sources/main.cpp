51de1d417de0cc5ed41395d0411b43c784e9ec19
Hvarz <hvarzahar@gmail.com>
2020-12-21 16:22:25 +0300

    add new CMakeLists.txt file for subdirectory picoSHA

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,10 @@
 // Copyright 2020 Your Name <your_email>
 
 #include <hash_researcher.hpp>
+#include <iostream>
+#include <thread>
+
+
+int main() {
+  std::cout << std::thread::hardware_concurrency() << std::endl;
+}
