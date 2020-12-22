9d4de9a120e6ceb52d56b4fc97350da9b7d764b1
Hvarz <hvarzahar@gmail.com>
2020-12-22 03:38:01 +0300

    add new copyright string

--- include/hash_researcher.hpp
+++ include/hash_researcher.hpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Zakhar Ushakov <hvarzahar@gmail.com>
 
 #ifndef INCLUDE_HASH_RESEARCHER_HPP_
 #define INCLUDE_HASH_RESEARCHER_HPP_
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Zakhar Ushakov <hvarzahar@gmail.com>
 
 #include <hash_researcher.hpp>
 
